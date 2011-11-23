edb14397196e8a912317063ed1fa1d3dd30a2451
Will Cosgrove <will@willcosgrove.com>
2011-11-23 00:32:21 -0600

    Compacted
    Moved DulyNoted::Base into DulyNoted

--- lib/duly_noted.rb
+++ lib/duly_noted.rb
@@ -1,9 +1,53 @@
 require "redis"
+require "uri"
+require "duly_noted/helpers"
 require "duly_noted/version"
-require "duly_noted/base"
 
 module DulyNoted
   extend self
+  include Helpers
+  
+  def track(metric_name, options={})
+    options = {generated_at: Time.now}.merge(options)
+    key = normalize(metric_name)
+    key << ":#{options[:for]}" if options[:for]
+    DulyNoted.redis.zadd key, options[:generated_at].to_f, "#{key}:#{options[:generated_at].to_f}:meta"
+    DulyNoted.redis.set "#{normalize(metric_name)}:#{options[:for]}:#{options[:ref_id]}", key if options[:ref_id] # set alias key
+    DulyNoted.redis.hmset "#{key}:#{options[:generated_at].to_f}:meta", options[:meta] if options[:meta] # set meta data
+  end
+  
+  def update(metric_name, ref_id, options={})
+    key = normalize(metric_name)
+    key << ":#{options[:for]}" if options[:for]
+    key << ":#{ref_id}"
+    real_key = DulyNoted.redis.get key
+    DulyNoted.redis.hmset real_key, options[:meta] if options[:meta] 
+  end
+  
+  def query(metric_name, options={})
+    key = normalize(metric_name)
+    key << ":#{options[:for]}" if options[:for]
+    if options[:time_start] && options[:time_end]
+      results = DulyNoted.redis.zrevrangebyscore(key, options[:time_start].to_f, options[:time_end].to_f).collect do |metric|
+        DulyNoted.redis.hgetall metric
+      end
+    else
+      results = DulyNoted.redis.zrevrange(key, 0, -1).collect do |metric|
+        DulyNoted.redis.hgetall metric
+      end
+    end
+    return results
+  end
+  
+  def count(metric_name, options={})
+    key = normalize(metric_name)
+    key << ":#{options[:for]}" if options[:for]
+    if options[:time_start] && options[:time_end]
+      return DulyNoted.redis.zcount(key, options[:time_start].to_f, options[:time_end].to_f)
+    else
+      return DulyNoted.redis.zcard(key)
+    end
+  end
   
   def redis=(url)
     @redis = nil
--- lib/duly_noted/helpers.rb
+++ lib/duly_noted/helpers.rb
@@ -3,5 +3,9 @@ module DulyNoted
     def key_encode(type)
       "#{type}:#{Time.now.to_f}"
     end
+    
+    def normalize(str)
+      str.downcase.gsub(/[^a-z0-9 ]/i, '').strip
+    end
   end
 end

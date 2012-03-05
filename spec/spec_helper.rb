a9991a79e8cfec8c0707e1497ede00c3906b7b24
Will Cosgrove <will@willcosgrove.com>
2012-03-05 03:06:48 -0600

    Added require_relative workaround

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.dirname(caller[0]), path.to_str)
+    end
+  end
+end
+
 require_relative "../lib/duly_noted"
 require 'chronic'
